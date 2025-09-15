3a7c91549836cf558285ffa9650fcb859cd560c8
WebQx <WebQx@users.noreply.github.com>
2025-09-15 23:04:07 +0000

    deploy: 40fdf5229d36422d36d8908cfd158674044391b8

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -30,7 +30,7 @@ const RESOURCES = {"canvaskit/skwasm.js": "1ef3ea3a0fec4569e5d531da25f34095",
 "assets/AssetManifest.bin.json": "a1fee2517bf598633e2f67fcf3e26c94",
 "assets/AssetManifest.bin": "0b0a3415aad49b6e9bf965ff578614f9",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
-"flutter_bootstrap.js": "d12161188d255e2bdfa2f0ace643b7b4",
+"flutter_bootstrap.js": "2b1040590a94435d178eae9379f53541",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "index.html": "3a22b05b32d1ab677238f8955312654a",
 "/": "3a22b05b32d1ab677238f8955312654a"};
