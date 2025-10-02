69ba3fdea46e436ad4d5a2a8b2f5037e46138c2a
Taha Ahmad <webqx2103@gmail.com>
2025-10-02 01:53:35 +0000

    Fix Flutter analyzer issues
    - Revert analysis_options.yaml to use flutter_lints package
    - Fix IntegrationTestWidgetsBinding reference in test_web.dart
    - Ensure all theme files use CardThemeData consistently

--- app/test_web.dart
+++ app/test_web.dart
@@ -1,9 +1,10 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:mmt/main.dart' as app;
 
 void main() {
-  IntegrationTestWidgetsBinding.ensureInitialized();
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   group('MMT Web App Tests', () {
     testWidgets('Login flow', (tester) async {
